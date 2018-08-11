b8e8d74603a4cd4906979b1e59ffd173bb42c96a
Jose Alejandro Rojano Crisson <jc@uninorte.edu.co>
2018-08-11 11:38:19 -0500

    Language changed to Spanish

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world\n');
+  res.end('Hola mundo\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
 });
